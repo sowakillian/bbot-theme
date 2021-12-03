92e4d7b27846c87433842d79bc7a7a92cb613a8f
Killian <ksowa@outlook.fr>
2021-12-03 19:14:36 +0100

    implement mails

--- resources/assets/scripts/routes/contact.js
+++ resources/assets/scripts/routes/contact.js
@@ -1,9 +1,28 @@
+// import request from 'request'
+
 export default {
     init() {
       this.initListClickListener();
+      this.sendMail();
     },
     finalize() {
       // JavaScript to be fired on the home page, after the init JS
+    },
+
+    sendMail() {
+      console.warn('I sent an email')
+      
+      fetch('https://api.sendinblue.com/v3/smtp/email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key': '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',
+        },
+        body: '{"sender":{"email":"ksowa@outlook.fr"},"to":[{"email":"ksowa@outlook.fr"}],"replyTo":{"email":"ksowa@outlook.fr"},"templateId":1, "params": {"firstname":"Jacky", "lastname":"Lefou"}}',
+      }).then(res => {
+        console.log('Request complete! response:', res);
+      });
+
     },
 
     initListClickListener() {
