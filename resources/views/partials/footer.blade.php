3d80f83d9c0d6d4946c9a5e18516af2ffdcff4fd
Killian <ksowa@outlook.fr>
2022-02-21 17:32:51 +0100

    add space

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
   <div class="wrap">
-    Copyright - <a href="https://www.greenbig.com">GreenBigSAS</a> -  <a href="{{ home_url('/') }}mentions-legales">&nbspMentions légales</a>
+    Copyright -&nbsp;<a href="https://www.greenbig.com">GreenBigSAS</a>&nbsp;-  <a href="{{ home_url('/') }}mentions-legales">&nbspMentions légales</a>
   </div>
 </footer>
